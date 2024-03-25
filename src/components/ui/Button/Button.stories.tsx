98851b125682be4c93b1562dd875c66c813d6b27
hiro08gh <hiro08gh@gmail.com>
2024-03-25 20:12:44 +0900

    fix: storybook button title

--- src/components/ui/Button/Button.stories.tsx
+++ src/components/ui/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { fn } from "@storybook/test";
 import { Button } from "./Button";
 
 const meta = {
-  title: "Example/bUtton",
+  title: "Example/Button",
   component: Button,
   parameters: {
     layout: "centered",
